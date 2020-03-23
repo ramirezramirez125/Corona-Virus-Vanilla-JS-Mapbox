fb10fa82799bab17803406bdd06fc9d10aaade96
Prateek Madaan <prince.cenation@gmail.com>
2020-03-23 23:35:15 +0530

    data normalization, populate the data for the country codes and the countries is a better approach rather than finding uniques and all that will add to time complexity and all.

--- app.js
+++ app.js
@@ -9,7 +9,177 @@ const coronaData = {
   latest: {},
   locations: []
 }
-
+const countriesWithCountryCodes = {
+  "TH": "Thailand",
+  "JP": "Japan",
+  "SG": "Singapore",
+  "NP": "Nepal",
+  "MY": "Malaysia",
+  "CA": "Canada",
+  "AU": "Australia",
+  "KH": "Cambodia",
+  "LK": "Sri Lanka",
+  "DE": "Germany",
+  "FI": "Finland",
+  "AE": "United Arab Emirates",
+  "PH": "Philippines",
+  "IN": "India",
+  "IT": "Italy",
+  "SE": "Sweden",
+  "ES": "Spain",
+  "BE": "Belgium",
+  "EG": "Egypt",
+  "LB": "Lebanon",
+  "IQ": "Iraq",
+  "OM": "Oman",
+  "AF": "Afghanistan",
+  "BH": "Bahrain",
+  "KW": "Kuwait",
+  "DZ": "Algeria",
+  "HR": "Croatia",
+  "CH": "Switzerland",
+  "AT": "Austria",
+  "IL": "Israel",
+  "PK": "Pakistan",
+  "BR": "Brazil",
+  "GE": "Georgia",
+  "GR": "Greece",
+  "MK": "North Macedonia",
+  "NO": "Norway",
+  "RO": "Romania",
+  "EE": "Estonia",
+  "SM": "San Marino",
+  "BY": "Belarus",
+  "IS": "Iceland",
+  "LT": "Lithuania",
+  "MX": "Mexico",
+  "NZ": "New Zealand",
+  "NG": "Nigeria",
+  "IE": "Ireland",
+  "LU": "Luxembourg",
+  "MC": "Monaco",
+  "QA": "Qatar",
+  "EC": "Ecuador",
+  "AZ": "Azerbaijan",
+  "AM": "Armenia",
+  "DO": "Dominican Republic",
+  "ID": "Indonesia",
+  "PT": "Portugal",
+  "AD": "Andorra",
+  "LV": "Latvia",
+  "MA": "Morocco",
+  "SA": "Saudi Arabia",
+  "SN": "Senegal",
+  "AR": "Argentina",
+  "CL": "Chile",
+  "JO": "Jordan",
+  "UA": "Ukraine",
+  "HU": "Hungary",
+  "LI": "Liechtenstein",
+  "PL": "Poland",
+  "TN": "Tunisia",
+  "BA": "Bosnia and Herzegovina",
+  "SI": "Slovenia",
+  "ZA": "South Africa",
+  "BT": "Bhutan",
+  "CM": "Cameroon",
+  "CO": "Colombia",
+  "CR": "Costa Rica",
+  "PE": "Peru",
+  "RS": "Serbia",
+  "SK": "Slovakia",
+  "TG": "Togo",
+  "MT": "Malta",
+  "MQ": "Martinique",
+  "BG": "Bulgaria",
+  "MV": "Maldives",
+  "BD": "Bangladesh",
+  "PY": "Paraguay",
+  "AL": "Albania",
+  "CY": "Cyprus",
+  "BN": "Brunei",
+  "US": "US",
+  "BF": "Burkina Faso",
+  "VA": "Holy See",
+  "MN": "Mongolia",
+  "PA": "Panama",
+  "CN": "China",
+  "IR": "Iran",
+  "KR": "Korea, South",
+  "FR": "France",
+  "XX": "Cruise Ship",
+  "DK": "Denmark",
+  "CZ": "Czechia",
+  "TW": "Taiwan*",
+  "VN": "Vietnam",
+  "RU": "Russia",
+  "MD": "Moldova",
+  "BO": "Bolivia",
+  "HN": "Honduras",
+  "GB": "United Kingdom",
+  "CD": "Congo (Kinshasa)",
+  "CI": "Cote d'Ivoire",
+  "JM": "Jamaica",
+  "TR": "Turkey",
+  "CU": "Cuba",
+  "GY": "Guyana",
+  "KZ": "Kazakhstan",
+  "ET": "Ethiopia",
+  "SD": "Sudan",
+  "GN": "Guinea",
+  "KE": "Kenya",
+  "AG": "Antigua and Barbuda",
+  "UY": "Uruguay",
+  "GH": "Ghana",
+  "NA": "Namibia",
+  "SC": "Seychelles",
+  "TT": "Trinidad and Tobago",
+  "VE": "Venezuela",
+  "SZ": "Eswatini",
+  "GA": "Gabon",
+  "GT": "Guatemala",
+  "MR": "Mauritania",
+  "RW": "Rwanda",
+  "LC": "Saint Lucia",
+  "VC": "Saint Vincent and the Grenadines",
+  "SR": "Suriname",
+  "XK": "Kosovo",
+  "CF": "Central African Republic",
+  "CG": "Congo (Brazzaville)",
+  "GQ": "Equatorial Guinea",
+  "UZ": "Uzbekistan",
+  "NL": "Netherlands",
+  "BJ": "Benin",
+  "LR": "Liberia",
+  "SO": "Somalia",
+  "TZ": "Tanzania",
+  "BB": "Barbados",
+  "ME": "Montenegro",
+  "KG": "Kyrgyzstan",
+  "MU": "Mauritius",
+  "ZM": "Zambia",
+  "DJ": "Djibouti",
+  "GM": "Gambia, The",
+  "BS": "Bahamas, The",
+  "TD": "Chad",
+  "SV": "El Salvador",
+  "FJ": "Fiji",
+  "NI": "Nicaragua",
+  "MG": "Madagascar",
+  "HT": "Haiti",
+  "AO": "Angola",
+  "CV": "Cape Verde",
+  "NE": "Niger",
+  "PG": "Papua New Guinea",
+  "ZW": "Zimbabwe",
+  "TL": "Timor-Leste",
+  "ER": "Eritrea",
+  "UG": "Uganda",
+  "DM": "Dominica",
+  "GD": "Grenada",
+  "MZ": "Mozambique",
+  "SY": "Syria"
+}
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJhdGVlazk1MSIsImEiOiJjazg0OHNpNnYxNnNoM2htczRncmgydXlsIn0._fOmIUUD2iZtHvzv0uWGKA'
 
 function renderMap() {
@@ -193,37 +363,38 @@ function populateLocation(country, country_code) {
   countryOption.textContent = `${country_code}-${country}`;
   countrySelectDropdown.appendChild(countryOption);
 }
-
 function populateLocations() {
-  console.log(coronaData.locations);
-  const countriesWithCodes = coronaData.locations.map(location => {
-    return {
-      country: location.country,
-      country_code: location.country_code
-    }
-  });
-  console.log(countriesWithCodes);
-  const uniqueCountriesWithCodes =
-    findUniqueCountriesWithCodes(countriesWithCodes, ['country', 'country_code']);
-   console.log(uniqueCountriesWithCodes);
-  uniqueCountriesWithCodes.forEach(({ country, country_code }) => populateLocation(country, country_code));
+  Object.entries(countriesWithCountryCodes).forEach(([country_code, country]) => populateLocation(country, country_code));
 }
+// The below approach is good but it would require some computation
+// that will take some time to render the data on the UI
+// Better is to create an object of all the locations up front.
 
-function findUniqueCountriesWithCodes(countriesWithCodes, propertyNames) {
-  const keysAndValues = countriesWithCodes.map(countryWithCode => {
-    const key = propertyNames.map(propertyName => countryWithCode[propertyName]).join('|');
-    return [key, countryWithCode];
-  });
-  const map = new Map(keysAndValues);
-  return Array.from(map.values());
-}
+// function populateLocations() {
+//   const countriesWithCodes = coronaData.locations
+//     .map(({ country, country_code }) =>  ({ country, country_code }));
+//   const uniqueCountriesWithCodes =
+//     findUniqueCountriesWithCodes(countriesWithCodes, ['country', 'country_code']);
+//   console.log(uniqueCountriesWithCodes);
+//   uniqueCountriesWithCodes.forEach(({ country, country_code }) => populateLocation(country, country_code));
+
+// }
+
+// function findUniqueCountriesWithCodes(countriesWithCodes, propertyNames) {
+//   const keysAndValues = countriesWithCodes.map(countryWithCode => {
+//     const key = propertyNames.map(propertyName => countryWithCode[propertyName]).join('|');
+//     return [key, countryWithCode];
+//   });
+//   const map = new Map(keysAndValues);
+//   return Array.from(map.values());
+// }
 
 async function initializeApp() {
   console.log('inside the init method');
   setReferences();
   doEventBindings();
-  await performAsyncCall();
   populateLocations();
+  await performAsyncCall();
   // console.log(coronaData.latest, coronaData.locations);
   renderMap();
 }
