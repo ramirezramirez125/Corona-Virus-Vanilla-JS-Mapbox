c6f402dde9987fc8f949a604b43fe41d221ac156
Prateek Madaan <prince.cenation@gmail.com>
2020-03-23 19:25:13 +0530

    added clusters for countries to represent the number of locations where corona virus spreaded across the world
    added clusters for countries to represent the number of locations where corona virus spreaded across the world 
    This is based on the data that I am getting from the API that I am reaching out to, it gives around 497 locations around the world and then you can click on a location and a table will appear telling the number of cases reported, number of deaths and the number of recovered persons from the clicked location. One thing that the API does not provide is the exact city, for each location it just shows the country and not the exact city name where the pandemic cases were observed. So there can be many clusters for a country. And you can zoom and zoom out each cluster to see specific details

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ latest: {},
 locations: []
 }
 
-const geoJSONCoronaData = [];
-
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJhdGVlazk1MSIsImEiOiJjazg0OHNpNnYxNnNoM2htczRncmgydXlsIn0._fOmIUUD2iZtHvzv0uWGKA'
 
 function renderMap() {
@@ -31,101 +29,157 @@ function renderMap() {
   // Add zoom and rotation controls to the map.
   map.addControl(new mapboxgl.NavigationControl());
 
-  coronaData.locations.forEach(location => {
-    geoJSONCoronaData.push({
-      "type": "Feature",
-      "properties": {
-        "description": `
-      <table>
-        <thead>
-          <tr>${location.country}</tr>
-        </thead>
-        <tbody>
-          <tr>
-             <td>Confirmed Cases:</td>
-             <td>${location.latest.confirmed}</td>
-          </tr>
-          <tr>
-             <td>Deaths:</td>
-             <td>${location.latest.deaths}</td>
-          </tr>
-          <tr>
-             <td>Recovered:</td>
-             <td>${location.latest.recovered}</td>
-          </tr>
-          <tr>
-             <td>Latitude:</td>
-             <td>${location.coordinates.latitude}</td>
-          </tr>
-          <tr>
-             <td>Longitude:</td>
-             <td>${location.coordinates.longitude}</td>
-          </tr>
-        </tbody>
-      </table>
-      `,
-        "icon": "rocket"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [`${location.coordinates.longitude}`,
-        `${location.coordinates.latitude}`]
-      }
-    });
-    // const marker = new mapboxgl.Marker()
-    //   .setLngLat([
-    //     `${location.coordinates.longitude}`,
-    //     `${location.coordinates.latitude}`
-    //   ]).addTo(map);
-    });
-    /// Listening for the load event on the map
-    map.on('load', function () {
-      // Add a layer showing the places.
-      map.addLayer({
-        "id": "places",
-        "type": "symbol",
-        "source": {
-          "type": "geojson",
-          "data": {
-            "type": "FeatureCollection",
-            "features": geoJSONCoronaData
+  map.on('load', function () {
+    map.addSource('places', {
+      "type": "geojson",
+      "data": {
+        "type": "FeatureCollection",
+        "crs": {
+          "type": "name",
+          "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
           }
         },
-        "layout": {
-          "icon-image": "{icon}-15",
-          'icon-size': 1,
-          "icon-allow-overlap": true
-        }
-      });
-
-      // When a click event occurs on a feature in the places layer, open a popup at the
-      // location of the feature, with description HTML from its properties.
-      map.on('click', 'places', function (event) {
-        const coordinates = event.features[0].geometry.coordinates.slice();
-        console.log(coordinates);
-        const description = event.features[0].properties.description;
-
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(event.lngLat.lng - coordinates[0]) > 180) {
-          coordinates[0] += event.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-
-        new mapboxgl.Popup().setLngLat(coordinates)
-          .setHTML(description)
-          .addTo(map);
-      });
-
-      // Change the cursor to a pointer when the mouse is over the places layer.
-      map.on('mouseenter', 'places', function () {
-        map.getCanvas().style.cursor = 'pointer';
-      });
-      // Change it back to a pointer when it leaves.
-      map.on('mouseleave', 'places', function () {
-        map.getCanvas().style.cursor = '';
-      });
+        "features": coronaData.locations.map(location => {
+          return {
+            "type": "Feature",
+            "properties": {
+            "description": `
+                <table>
+                  <thead>
+                    <tr>${location.country}</tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Confirmed Cases:</td>
+                      <td>${location.latest.confirmed}</td>
+                    </tr>
+                    <tr>
+                      <td>Deaths:</td>
+                      <td>${location.latest.deaths}</td>
+                    </tr>
+                    <tr>
+                      <td>Recovered:</td>
+                      <td>${location.latest.recovered}</td>
+                    </tr>
+                    <tr>
+                      <td>Latitude:</td>
+                      <td>${location.coordinates.latitude}</td>
+                    </tr>
+                    <tr>
+                      <td>Longitude:</td>
+                      <td>${location.coordinates.longitude}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                `,
+              "icon": "rocket"
+            },
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                `${location.coordinates.longitude}`,
+                `${location.coordinates.latitude}`
+              ]
+            }
+          }
+        })
+      },
+      cluster: true,
+      clusterMaxZoom: 14,
+      clusterRadius: 50
     });
+    map.addLayer({
+      id: 'clusters',
+      type: 'circle',
+      source: 'places',
+      filter: ['has', 'point_count'],
+      paint: {
+        'circle-color': [
+          'step',
+          ['get', 'point_count'],
+          '#51bbd6',
+          100,
+          '#f1f075',
+          750,
+          '#f28cb1'
+        ],
+        'circle-radius': [
+          'step',
+          ['get', 'point_count'],
+          20,
+          100,
+          30,
+          750,
+          40
+        ]
+      }
+    });
+
+    map.addLayer({
+      id: 'cluster-count',
+      type: 'symbol',
+      source: 'places',
+      filter: ['has', 'point_count'],
+      layout: {
+        'text-field': '{point_count_abbreviated}',
+        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+        'text-size': 12
+      }
+    });
+    map.addLayer({
+      id: 'unclustered-point',
+      type: 'circle',
+      source: 'places',
+      filter: ['!', ['has', 'point_count']],
+      paint: {
+        'circle-color': '#11b4da',
+        'circle-radius': 4,
+        'circle-stroke-width': 1,
+        'circle-stroke-color': '#fff'
+      }
+    });
+    // inspect a cluster on the click
+    map.on('click', 'clusters', function (event) {
+      const features = map.queryRenderedFeatures(event.point, {
+        layers: ['clusters']
+      });
+      const clusterId = features[0].properties.cluster_id;
+      map.getSource('places').getClusterExpansionZoom(
+        clusterId,
+        function (error, zoom) {
+          if (error) return;
+          map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom
+          });
+        }
+      );
+    });
+    map.on('click', 'unclustered-point', function (event) {
+      const coordinates = event.features[0].geometry.coordinates.slice();
+      // console.log(coordinates);
+      const { description, icon } = event.features[0].properties;
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(event.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += event.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      new mapboxgl.Popup().setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
+    });
+    //   // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'clusters', function () {
+      map.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'clusters', function () {
+      map.getCanvas().style.cursor = '';
+    });
+    })
     // console.log(geoJSONCoronaData);
 }
 
@@ -134,7 +188,7 @@ async function initializeApp() {
   setReferences();
   doEventBindings();
   await performAsyncCall();
-  console.log(coronaData.latest, coronaData.locations);
+  // console.log(coronaData.latest, coronaData.locations);
   renderMap();
 }
 
